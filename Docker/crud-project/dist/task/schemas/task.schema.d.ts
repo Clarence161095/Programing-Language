4cd213ca63978a44abc08b66b6abcc5fd8d826ac
Clarence161095 <nguyenanhtuan161095@gmail.com>
2021-09-23 21:21:37 +0700

    update docker-compose issue volumes

--- Docker/crud-project/dist/task/schemas/task.schema.d.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-export declare type TaskDocument = Task & Document;
-export declare class Task {
-    name: string;
-    description: string;
-}
-export declare const TaskSchema: import("mongoose").Schema<Document<Task, any, any>, import("mongoose").Model<Document<Task, any, any>, any, any>, {}>;

edfd71841b67a86cd2ba40927035c3996b6cd603
Clarence161095 <nguyenanhtuan161095@gmail.com>
2021-10-03 10:55:28 +0700

    update new source

--- Javascript/hello-world.js
+++ Javascript/hello-world.js
@@ -1,10 +1,13 @@
-const test = ["test", "test2", "test3", "test4"];
-
-const convertObjectToString = (input) => {
-  return input.reduce((acc, val, index) => {
-    acc[index] = val;
-    return acc;
-  }, {});
+var isDate = function (date) {
+  const newDate = new Date(date);
+  return newDate !== "Invalid Date" && !isNaN(newDate);
 };
 
-console.log(convertObjectToString(test));
+const today = new Date("2021-09-30").getTime();
+console.log(today);
+console.log(Date.now());
+
+// let e = "today";
+// let dateString = "1968-11-16T00:00:00";
+// let newDate = new Date(e);
+// console.log(newDate);

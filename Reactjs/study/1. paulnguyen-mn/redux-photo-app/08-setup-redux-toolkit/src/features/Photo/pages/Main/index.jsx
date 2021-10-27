696692eaed93b79e3843855ac97f99d2177629e8
Nguyễn Anh Tuấn <tuan.nguyen@napaglobal.com>
2021-10-27 17:01:40 +0700

    update

---
+++ Reactjs/study/1. paulnguyen-mn/redux-photo-app/08-setup-redux-toolkit/src/features/Photo/pages/Main/index.jsx
@@ -0,0 +1,25 @@
+import Banner from 'components/Banner';
+import Images from 'constants/images';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container } from 'reactstrap';
+import { useSelector } from 'react-redux';
+
+MainPage.propTypes = {};
+
+function MainPage(props) {
+  const photos = useSelector(state => state.photos);
+  console.log('List of photos: ', photos);
+
+  return (
+    <div className="photo-main">
+      <Banner title="Your awesome photos 🎉" backgroundUrl={Images.PINK_BG} />
+
+      <Container className="text-center">
+        <Link to="/photos/add">Add new photo</Link>
+      </Container>
+    </div>
+  );
+}
+
+export default MainPage;

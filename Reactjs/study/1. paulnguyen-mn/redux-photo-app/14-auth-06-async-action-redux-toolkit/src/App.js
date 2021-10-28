c1fc17ffb1d5e2bbc28d63bbdc0ae93197c7c2a3
Nguyễn Anh Tuấn <tuan.nguyen@napaglobal.com>
2021-10-28 16:24:06 +0700

    update

--- Reactjs/study/1. paulnguyen-mn/redux-photo-app/14-auth-06-async-action-redux-toolkit/src/App.js
+++ Reactjs/study/1. paulnguyen-mn/redux-photo-app/14-auth-06-async-action-redux-toolkit/src/App.js
@@ -1,18 +1,18 @@
-import { unwrapResult } from '@reduxjs/toolkit';
-import productApi from 'api/productApi';
-import { getMe } from 'app/userSlice';
-import SignIn from 'features/Auth/pages/SignIn';
-import firebase from 'firebase';
-import React, { Suspense, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import { Button } from 'reactstrap';
-import './App.scss';
-import Header from './components/Header';
-import NotFound from './components/NotFound';
+import { unwrapResult } from "@reduxjs/toolkit";
+import productApi from "api/productApi";
+import { getMe } from "app/userSlice";
+import SignIn from "features/Auth/pages/SignIn";
+import firebase from "firebase";
+import React, { Suspense, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { Button } from "reactstrap";
+import "./App.scss";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
 
 // Lazy load - Code splitting
-const Photo = React.lazy(() => import('./features/Photo'));
+const Photo = React.lazy(() => import("./features/Photo"));
 
 // Configure Firebase.
 const config = {
@@ -22,6 +22,7 @@ const config = {
 firebase.initializeApp(config);
 
 function App() {
+  // eslint-disable-next-line
   const [productList, setProductList] = useState([]);
   const dispatch = useDispatch();
 
@@ -31,41 +32,44 @@ function App() {
         const params = {
           _page: 1,
           _limit: 10,
-
         };
         const response = await productApi.getAll(params);
         console.log(response);
         setProductList(response.data);
       } catch (error) {
-        console.log('Failed to fetch product list: ', error);
+        console.log("Failed to fetch product list: ", error);
       }
-    }
+    };
 
     fetchProductList();
   }, []);
 
   // Handle firebase auth changed
   useEffect(() => {
-    const unregisterAuthObserver = firebase.auth().onAuthStateChanged(async (user) => {
-      if (!user) {
-        // user logs out, handle something here
-        console.log('User is not logged in');
-        return;
-      }
+    const unregisterAuthObserver = firebase
+      .auth()
+      .onAuthStateChanged(async (user) => {
+        if (!user) {
+          // user logs out, handle something here
+          console.log("User is not logged in");
+          return;
+        }
 
-      // Get me when signed in
-      // const action = getMe();
-      try {
-        const actionResult = await dispatch(getMe());
-        const currentUser = unwrapResult(actionResult);
-        console.log('Logged in user: ', currentUser);
-      } catch (error) {
-        console.log('Failed to login ', error.message);
-        // show toast error
-      }
-    });
+        // Get me when signed in
+        // const action = getMe();
+        try {
+          const actionResult = await dispatch(getMe());
+          // Get payload của dispatch
+          const currentUser = unwrapResult(actionResult);
+          console.log("Logged in user: ", currentUser);
+        } catch (error) {
+          console.log("Failed to login ", error.message);
+          // show toast error
+        }
+      });
 
     return () => unregisterAuthObserver();
+    // eslint-disable-next-line
   }, []);
 
   const handleButtonClick = async () => {
@@ -73,15 +77,13 @@ function App() {
       const params = {
         _page: 1,
         _limit: 10,
-
       };
       const response = await productApi.getAll(params);
       console.log(response);
     } catch (error) {
-      console.log('Failed to fetch product list: ', error);
+      console.log("Failed to fetch product list: ", error);
     }
-  }
-
+  };
 
   return (
     <div className="photo-app">

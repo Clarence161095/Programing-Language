e7374ede1089f1c376ad835950f0e19df6f4e2e4
Nguyễn Anh Tuấn <tuan.nguyen@napaglobal.com>
2021-10-28 10:59:34 +0700

    update

--- Reactjs/study/1. paulnguyen-mn/redux-photo-app/11-add-api-module/src/App.js
+++ Reactjs/study/1. paulnguyen-mn/redux-photo-app/11-add-api-module/src/App.js
@@ -1,14 +1,15 @@
-import React, { Suspense, useEffect, useState } from 'react';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import './App.scss';
-import Header from './components/Header';
-import NotFound from './components/NotFound';
-import productApi from 'api/productApi';
+import React, { Suspense, useEffect, useState } from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import "./App.scss";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+import productApi from "api/productApi";
 
 // Lazy load - Code splitting
-const Photo = React.lazy(() => import('./features/Photo'));
+const Photo = React.lazy(() => import("./features/Photo"));
 
 function App() {
+  // eslint-disable-next-line
   const [productList, setProductList] = useState([]);
 
   useEffect(() => {
@@ -17,19 +18,17 @@ function App() {
         const params = {
           _page: 1,
           _limit: 10,
-
         };
         const response = await productApi.getAll(params);
         console.log(response);
         setProductList(response.data);
       } catch (error) {
-        console.log('Failed to fetch product list: ', error);
+        console.log("Failed to fetch product list: ", error);
       }
-    }
+    };
 
     fetchProductList();
   }, []);
-
 
   return (
     <div className="photo-app">

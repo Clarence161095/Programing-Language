4fa65f5bf2a656be2d332ec5763c3f977a5a5fec
Nguyễn Anh Tuấn <tuan.nguyen@napaglobal.com>
2021-10-28 14:06:38 +0700

    update

---
+++ Reactjs/study/1. paulnguyen-mn/redux-photo-app/12-auth-04-google-login-via-firebase-ui/src/features/Auth/pages/SignIn/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+SignIn.propTypes = {};
+
+const uiConfig = {
+  signInFlow: "redirect",
+  signInSuccessUrl: "/photos",
+  signInOptions: [
+    // Nếu muốn đăng nhập thêm Facebook...ect thì config thêm ở đây
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+};
+
+function SignIn() {
+  return (
+    <div>
+      <div className="text-center">
+        <h2>Login Form</h2>
+
+        <p>or login with social accounts</p>
+      </div>
+
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+}
+
+export default SignIn;
